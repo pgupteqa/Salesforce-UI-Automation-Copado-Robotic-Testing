bc694ddc0345ad22f129bc22fb1d5563bf5152a1
Pratik G undefined <gupte.pratik024@gmail.com>
2026-08-08 17:05:26 +0000

    Testfile

---
+++ resources/variables/test_data.py
@@ -0,0 +1,11 @@
+class Test_Data:
+
+    def get_input_data(self):
+        data_map = {
+
+            "Client Response Pending" : "Pending Requestor",
+            "Return To Submitter": "In Progress"
+        }
+
+        return data_map.get({})
+
