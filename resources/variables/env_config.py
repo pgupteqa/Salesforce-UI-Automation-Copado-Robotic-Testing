cd6bfc577141d8115f022b1d04045dfc55b510bf
Pratik G undefined <gupte.pratik024@gmail.com>
2026-08-09 19:01:53 +0000

    test

--- resources/variables/env_config.py
+++ resources/variables/env_config.py
@@ -1,10 +1,13 @@
 dev_env = {'sf_base_url': 'https://orgfarm-0eaed58a8f-dev-ed.develop.my.salesforce.com'}
 qa_env = {'sf_base_url': 'https://orgfarm-0eaed58a8f-dev-ed.develop.my.salesforce.com'}
+prod_env = {'sf_base_url': 'https://login.salesforce.com'}
 
 def get_variables(env):
     if env == 'dev_env':
         return dev_env
     elif env == 'qa_env':
         return qa_env
+    elif env == 'prod_env':
+        return prod_env
     else:
         return dev_env
