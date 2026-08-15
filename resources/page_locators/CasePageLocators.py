4bedf3348e564264bf4ec919c6394184087f970e
Pratik G undefined <gupte.pratik024@gmail.com>
2026-08-15 12:36:34 +0000

    Added the account page locator

---
+++ resources/page_locators/CasePageLocators.py
@@ -0,0 +1,14 @@
+class CasePage_Locators(object):
+
+    def __init__(self):
+        pass
+
+    def get_variable(self):
+
+        LOCATORS = {
+
+            ""
+
+        }
+
+        return LOCATORS
