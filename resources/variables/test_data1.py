45f46c434467263e3a09a252b45e80f3c9f8f92a
Pratik G undefined <gupte.pratik024@gmail.com>
2026-08-23 17:01:17 +0000

    test

--- resources/variables/test_data1.py
+++ resources/variables/test_data1.py
@@ -1,12 +1,7 @@
-    def get_variable(self, environment, scenario_variable):
-        """
-        Get scenario Variables
-        
-        :return: scenario variables
-        """
-        #global env
-        #env = environment
-        data_map ={
+def get_variable(self, environment, scenario_variable):
+ 
+    data_map =
+    {
         
         "stg_env.create_account":
         {
@@ -22,23 +17,13 @@
 
         "stg_env.create_case":
         {
-              "subject": "Unable to access billing invoice",
-              "description": "Customer reports a 404 error when downloading the latest invoice PDF from the client portal.",
-              "priority": "High",
-              "origin": "Phone"
+            "subject": "Unable to access billing invoice",
+            "description": "Customer reports a 404 error when downloading the latest invoice PDF from the client portal.",
+            "priority": "High",
+            "origin": "Phone"
         },
+    }
 
-
-        #'env': env
-        }
-
-            #Read Data map variable
-            #variable_name = self._get_environment() + '.' + scenario_variable
-
-            #if data_map.get(variable_name) != None:
-            #    return data_map.get(variable_name)
-            #else:
-            #    return data_map.get(scenario_variable)
     variable_name = f"{environment.lower()}.{scenario_variable}"
     data = data_map.get(variable_name, {})
 
