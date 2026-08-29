07ef22aedfb63bd9b6d3469383a6d156229df0a4
Pratik G undefined <gupte.pratik024@gmail.com>
2026-08-29 11:58:00 +0000

    test

--- resources/page_locators/CasePageLocators.py
+++ resources/page_locators/CasePageLocators.py
@@ -1,14 +1,8 @@
-def get_variable():
+*** Variables ***
 
-    LOCATORS = {
-
-        "CASE_NEW_BUTTON": "xpath=//a[@title='New']",
-        "CASE_SUBJECT": "xpath=//input[@name='Subject']",
-        "CASE_DESCRIPTION": "xpath=//input[@name='Description']",
-        "CASE_SAVE_BUTTON": "xpath=//button[@name='SaveEdit']",
-        "CASENUMBER_TOAST": "xpath=//div//span[contains(@class,'toastMessage')]/a",
-        "CASESEARCH_RESULT": "xpath=//th//a[contains(text(),'dynamic_value')]"
-
-    }
-
-    return LOCATORS
+${CASE_NEW_BUTTON} xpath=//a[@title='New']
+${CASE_SUBJECT} xpath=//input[@name='Subject']
+${CASE_DESCRIPTION} xpath=//input[@name='Description']
+${CASE_SAVE_BUTTON} xpath=//button[@name='SaveEdit']
+${CASENUMBER_TOAST} xpath=//div//span[contains(@class,'toastMessage')]/a
+${CASESEARCH_RESULT} xpath=//th//a[contains(text(),'dynamic_value')]
